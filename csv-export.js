281a45468eb445f1855b58b6f0074cbd5b371d63
LGraber <leegraber@hotmail.com>
2017-10-12 08:25:16 -0700

    Code runs end-to-end but doesn't work

--- csv-export.js
+++ csv-export.js
@@ -39,16 +39,80 @@ function onBtnClick() {
 }
 
 function addFields() {
-    const fields = document.getElementById('field-list');
-    for (let i=0; i<5; i++) {
-        const li = document.createElement('li');
-        li.classList.add('list-group-item');
-        li.appendChild(document.createTextNode('Hello' + i));
-        fields.appendChild(li);
-    }
-    makeCheckboxListPretty();
+    let worksheetIndex = document.getElementById('sheet-names').selectedIndex;
+    tableau.addIn.dashboardContent.dashboard.worksheets[worksheetIndex].getUnderlyingDataAsync({maxRows: 1}).then(function (dataTable) {
+        const fields = document.getElementById('field-list');
+        for(let col of dataTable.columns) {
+            const li = document.createElement('li');
+            li.classList.add('list-group-item');
+            li.appendChild(document.createTextNode(col.fieldName));
+            fields.appendChild(li);
+        }
+        makeCheckboxListPretty();
+    }, function (err) {
+      alert('getUnderlyingDataAsync failed: ' + err.toString());
+    });
 }
 
+function onSaveClicked() {
+    let worksheetIndex = document.getElementById('sheet-names').selectedIndex;
+    tableau.addIn.dashboardContent.dashboard.worksheets[worksheetIndex].getUnderlyingDataAsync().then(function (dataTable) {
+
+        let selectedColumns = new Array(dataTable.columns.length);
+        let fields = document.querySelectorAll('#field-list li');
+        for(let idx=0; idx<fields.length; ++idx) {
+            if (fields[idx].classList.contains('active')){
+                selectedColumns[idx] = 1;
+            } else {
+                selectedColumns[idx] = 0;
+            }
+        }
+
+        // The data should always be a 2d array. We will join
+        let csvContent = '\ufeff'
+        for(let i=0; i<dataTable.data.length; ++i) {
+            for(let j=0; j<dataTable.columns.length; ++j) {
+                if (selectedColumns[j] == 1) {
+                    csvContent += '"' + dataTable.data[i][j].formattedValue + '"';
+                    if (j != dataTable.columns.length - 1) {
+                        csvContent += ","
+                    }
+                }
+            }
+            csvContent += "\n";
+        }
+        
+        let blob = new Blob([csvContent],
+                    {type: 'text/csv;charset=utf-8' });
+
+        if (window.navigator.msSaveBlob) {
+            window.navigator.msSaveBlob(blob, "data.csv");
+        } else {
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = "data.csv";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }, function (err) {
+      alert('getUnderlyingDataAsync failed: ' + err.toString());
+    });
+}
 function onBodyLoad() {
-    document.getElementById('sheet-names').addEventListener('change', addFields);
+    tableau.addIn.initializeAsync().then(function () {
+        const selectedSheet = tableau.addIn.settings.get('sheetName');
+        let allSheets = tableau.addIn.dashboardContent.dashboard.worksheets;
+        for (const sheet of allSheets) {
+          let opt = document.createElement('option');
+          opt.value = opt.text = sheet.name;
+          if (sheet.name === selectedSheet) {
+            opt.setAttribute('selected', 'selected');
+          }
+          document.getElementById('sheet-names').appendChild(opt);
+        }
+    
+        document.getElementById('sheet-names').addEventListener('change', addFields);
+        addFields();
+    });
 }
