c2a83c374767f1a3de085e085f709cd863c98b29
Lee Graber <lgraber@lgraber-mac3.tsi.lan>
2017-10-12 02:01:27 -0700

    more updates

--- csv-export.js
+++ csv-export.js
@@ -3,7 +3,12 @@ function downloadContentToCsv(opts) {
         throw 'Where is the data?';
     }
  
-    var blob = new Blob(['\ufeff' + opts.content.join(';')],
+    // The data should always be a 2d array. We will join
+    let csvContent = '\ufeff'
+    for (i=0; i<opts.content.length; ++i) {
+        csvContent += opts.content[i].join(',') + '\n';
+    }
+    var blob = new Blob([csvContent],
                  {type: 'text/csv;charset=utf-8' });
 
     if (window.navigator.msSaveBlob) {
@@ -19,10 +24,31 @@ function downloadContentToCsv(opts) {
 }
 
 function onBtnClick() {
+    const data = new Array(5);
+    for (let i=0; i<5; i++) {
+        data[i] = new Array(5);
+        for (let j=0; j<5; ++j) {
+            let val = i+j;
+            data[i][j] = val.toString();
+        }
+    }
     downloadContentToCsv({
         filename: 'data.csv',
-        content: ['Hello', 'World', 'How are you']
+        content: data
     });
 }
 
+function addFields() {
+    const fields = document.getElementById('field-list');
+    for (let i=0; i<5; i++) {
+        const li = document.createElement('li');
+        li.classList.add('list-group-item');
+        li.appendChild(document.createTextNode('Hello' + i));
+        fields.appendChild(li);
+    }
+    makeCheckboxListPretty();
+}
 
+function onBodyLoad() {
+    document.getElementById('sheet-names').addEventListener('change', addFields);
+}
